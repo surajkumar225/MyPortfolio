c0bfdd24df1da5ce651a7a660ecf7dfe556b9285
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2023-07-07 16:39:19 +0530

    Surk

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,23 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">My Projects</h1>
+        <p className="pl-desc">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequuntur dolore excepturi. Odio quasi tempore totam, facilis nam eaque similique.
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://play-lh.googleusercontent.com/ZyWNGIfzUyoajtFcD7NhMksHEZh37f-MkHVGr5Yfefa-IX7yj9SMfI82Z7a2wpdKCA",
+    link: "http://surk.com",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "http://surk.com",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://surk.com",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://surk.com",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://surk.com",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://surk.com",
+  },
+];
